763ef44ec4f9c53a44da75a4978c9589b3cd5de0
Eric Bandin <ericmbandin@gmail.com>
2019-12-12 17:56:34 -0500

    Update CheeseDbContext.cs

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -10,6 +10,6 @@ namespace CheeseMVC.Data
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
             : base(options)
         { }
-
+       public DbSet<CheeseCategory> Categories { get; set; }
     }
 }
